049d175c6bada93fd6ad38022519298ede1bf4d3
YURY <yurytl@MacBook-Air-YURY.local>
2018-06-22 17:51:14 +0300

    Updated everything (Added axes, linear graph, legend)

--- index.js
+++ index.js
@@ -4,86 +4,130 @@ var dataset = [
     {income: 45, outcome: 25, date: '3 august'},
     {income: 75, outcome: 40, date: '4 august'},
     {income: 50, outcome: 30, date: '5 august'},
-    {income: 75, outcome: 40, date: '4 august'}
+    {income: 50, outcome: 45, date: '6 august'},
+    {income: 75, outcome: 20, date: '7 august'},
+    {income: 80, outcome: 60, date: '8 august'},
+    {income: 50, outcome: 25, date: '9 august'},
 ];
 
-var linearData = [];
-var incomeArr = [];
-var outcomeArr = [];
+var incomeArr = [],
+  linearData = [];
 
-var svgHeight = 200;
-var svgWidth = 500;
+var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.select('body')
-  .insert('svg');
+var x0 = d3.scaleBand()
+    .rangeRound([0, width])
+    // .paddingInner(0.4);
 
-var svg = d3.select('svg')
-  .attr('width', svgWidth)
-  .attr('height', svgHeight)
-    .style('transform', 'rotateX(180deg)');
+var x1 = d3.scaleBand()
+    .padding(0.7);
 
-var groupWidth = svgWidth / dataset.length;
-var spaceBetweenGroups = 10;
-var chartsWidth = groupWidth - ( 2 * spaceBetweenGroups );
-var chartWidth = chartsWidth / 2;
+var y = d3.scaleLinear()
+    .rangeRound([height, 0]);
+
+var z = d3.scaleOrdinal()
+    .range(["#98abc5", "#a05d56",]);
+
+x0.domain(dataset.map(function(d) { return d.date; }));
+x1.domain([0, dataset.length]).rangeRound([0, x0.bandwidth()]);
 
 for (var i = 0; i < dataset.length; i++) {
   incomeArr.push(dataset[i].income);
-  outcomeArr.push(dataset[i].outcome);
-  linearData.push({y: dataset[i].income, x: (i * groupWidth) + spaceBetweenGroups });
-  linearData.push({y: dataset[i].outcome, x: (i * groupWidth) + spaceBetweenGroups + chartsWidth });
+  linearData.push({y: dataset[i].income / 2, x: i * x0.bandwidth()});
+    linearData.push({y: dataset[i].outcome / 2, x: i * x0.bandwidth() + 2 * x1.bandwidth()});
 }
 
-var yScale = d3.scaleLinear()
-    .domain([0, d3.max(incomeArr)])
-    .range([0, svgHeight]);
+y.domain([0, d3.max(incomeArr)]).nice();
 
-var xScale = d3.scaleLinear()
-    .domain([0, dataset.length])
-    .range([0, svgWidth]);
+var gR = g.append("g")
+      .attr("transform", "translate(" + (x0(dataset[1].date)/2 - x1.bandwidth()) + ",0)")
+    .selectAll("g")
+    .data(dataset)
+    .enter().append("g")
+        .attr("transform", function(d) { return "translate(" + x0(d.date) + ",0)"; });
 
-var x_axis = d3.axisBottom().scale(xScale);
+    gR.append("rect")
+        .attr("x", function(d, i) { return 0; })
+        .attr("y", function(d) { return y(d.income); })
+        .attr("width", x1.bandwidth())
+        .attr("height", function(d) { return height - y(d.income); })
+        .attr("fill", function(d, i) { return z(0); });
+    gR.append("rect")
+        .attr("x",  x1.bandwidth())
+        .attr("y", function(d) { return y(d.outcome); })
+        .attr("width", x1.bandwidth())
+        .attr("height", function(d) { return height - y(d.outcome); })
+        .attr("fill", function(d, i) { return z(1); });
 
-var y_axis = d3.axisLeft().scale(yScale);
+    g.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x0))
 
-var gR = svg.selectAll('g')
-  .data(dataset)
-  .enter()
-  .append('g')
-    .style('padding', 10)
-    .style('transform', function(d, i) {
-        return 'translateX(' + ((i * (groupWidth)) + spaceBetweenGroups) + 'px)';
-    })
-
-gR.append('rect')
-  .attr('height', function(d, i) {
-        return yScale(d.income) ;
-  })
-  .attr('width', chartWidth)
-    .style('fill', '#5c9a15')
-
-gR.append('rect')
-  .attr('height', function(d, i) {
-    return yScale(d.outcome);
-  })
-  .attr('width', chartWidth)
-    .style('fill', '#d3323980')
-    .style('transform', 'translateX('+ chartWidth +'px)');
-
-svg.append("g")
-    .attr("transform", "translate(0, 0)")
-    .call(y_axis);
-
-svg.append("g")
-    .attr("transform", "translate(0, 0)")
-    .call(x_axis);
+    g.append("g")
+        .attr("class", "axis")
+      .call(d3.axisLeft(y).ticks(null, "s"))
+    .append("text")
+        .attr("x", -50)
+        .attr("y", y(y.ticks().pop()) - 15.5)
+        .attr("dy", "0.32em")
+        .attr("fill", "#000")
+        .attr("font-weight", "bold")
+        .attr("text-anchor", "start")
+      .text("MONEY MONEY MONEY");
 
 var lineFunction = d3.line()
-    .x(function(d, i) { return d.x })
-    .y(function(d) { return yScale(d.y)})
+    .x(function(d, i) { return d.x})
+    .y(function(d) { return y(d.y)})
 
-var lineGraph = svg.append("path")
-            .attr("d", lineFunction(linearData))
-            .attr("stroke", "blue")
-            .attr("stroke-width", 2)
-            .attr("fill", "none");
+var lineGraph = svg.select('g').select('g')
+  .append("path")
+      .attr("d", lineFunction(linearData))
+      .attr("stroke", "blue")
+      .attr("stroke-width", 1)
+      .attr("fill", "none");
+
+d3.select('body')
+  .append('div')
+    .attr('class', 'legend')
+    .attr('id', 'legendId')   
+
+var legend = d3.select('body')
+  .select('div.legend')
+
+var dot = svg
+  .select('g')
+  .select('g')
+  .selectAll("dot")
+    .data(linearData)
+    .enter().append("circle")
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return y(d.y); })
+      .attr("class", "dot")
+    .on("mouseover", function(d){
+      var x = this.getAttribute('cx');
+      var y = this.getAttribute('cy');
+
+      document.getElementById('legendId').innerHTML = '<div>x :' + x + '</div><div> y: ' + y + '</div>';
+
+      var legendPromise = new Promise( function(resolve, reject) {
+        resolve( legend.style('display', 'flex') )
+      })
+
+      legendPromise.then(function(resolve) {
+        legend.style('left', + x + 30 + 'px' )
+        .style('top', + y - 45 + 'px')
+        .style('opacity', '1')
+      })
+
+    })
+    .on("mouseout", function(d){
+      legend.style('opacity', '0')
+      setTimeout(function() {
+        legend.style('display', 'none')
+      }, 300)
+    })
